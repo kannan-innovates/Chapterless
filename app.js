7b6d2a38aba9a68c90f8207d91a298c09bc0ec5e
Kannan S <kannans02001@gmail.com>
2025-04-18 21:13:34 +0530

    Updated landiing Page and made partials for seperation and a 404 page is also added

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express  = require("express");
+const express = require("express");
 const app = express();
+const path = require("path");
 const env = require("dotenv").config();
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
+const userRouter = require("./routes/userRouter")
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", [
+  path.join(__dirname, "views/user"),
+  path.join(__dirname, "views/admin"),
+]);
+app.use(express.static(path.join(__dirname,"public")));
 
 
-connectDB()
 
-app.listen(process.env.PORT,() =>{
-     console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
-      
-})
+app.use("/",userRouter)
 
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
 
 module.exports.app;
