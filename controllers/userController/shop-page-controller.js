88250b2084eb534c6b7b467bf7619bba427e7d37
Kannan S <kannans02001@gmail.com>
2025-05-01 00:00:41 +0530

    Shop page done

--- controllers/userController/shop-page-controller.js
+++ controllers/userController/shop-page-controller.js
@@ -1,8 +1,15 @@
+const Product = require('../../models/productSchema');
+const Category = require('../../models/categorySchema');
+
 const shopPage = async (req,res) => {
      try {
-          res.render('shop-page')
+          const categories = await Category.find({isListed: true});
+          const products = await Product.find({isListed: true}).populate('category');
+          return res.render('shop-page',{categories, products});
+       
      } catch (error) {
-          console.log(`Error in rendering shop Page ${error}`)
+          console.log(`Error in rendering shop Page ${error}`);
+          res.status(500).send("Server Error");
      }
 }
 
